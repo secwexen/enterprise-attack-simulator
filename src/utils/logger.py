82a6b523e4074b26e5b9bbb321c9638e4de58963
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-03 12:07:04 +0300

    Update logger.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,15 @@
+from loguru import logger as _logger
+import sys
 
+
+_logger.remove()
+_logger.add(
+    sys.stdout,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+           "<level>{level: <8}</level> | "
+           "<cyan>{name}</cyan>:<cyan>{function}</cyan> - "
+           "<level>{message}</level>",
+    level="INFO",
+)
+
+logger = _logger
